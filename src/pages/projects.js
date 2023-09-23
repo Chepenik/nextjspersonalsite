cfd2562786bcaaf59692756fc49cbc2fe588038d
Conor <chepenikconor@gmail.com>
2023-09-22 23:47:28 -0400

    gave proper naming to projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,11 +5,11 @@ import { motion } from "framer-motion";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import proj1 from "../../public/images/projects/crypto-screener-cover-image.jpg";
-import proj2 from "../../public/images/projects/nft-collection-website-cover-image.jpg";
+import proj1 from "../../public/images/projects/texas-slim.jpg";
+import proj2 from "../../public/images/projects/bigHandWins.jpg";
 import proj3 from "../../public/images/projects/devdreaming.jpg";
-import proj4 from "../../public/images/projects/portfolio-cover-image.jpg";
-import proj5 from "../../public/images/projects/agency-website-cover-image.jpg";
+import proj4 from "../../public/images/projects/Caribou.jpg";
+import proj5 from "../../public/images/projects/Father.jpg";
 import proj6 from "../../public/images/projects/Marks.jpg";
 import TransitionEffect from "@/components/TransitionEffect";
 
