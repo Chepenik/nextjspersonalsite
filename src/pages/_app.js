669dd029ad2b3b9c07039b48fd28aad067c74565
Conor <chepenikconor@gmail.com>
2023-10-05 16:29:34 -0400

    tried adding google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,32 +2,51 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
 import { AnimatePresence } from "framer-motion";
+import dotenv from 'dotenv';
+dotenv.config();
+
 // pages/_app.js
-import { Montserrat } from "next/font/google";
+import { Montserrat } from "next/font/google";  
 import Head from "next/head";
 import { useRouter } from "next/router";
+import Script from "next/script";
 
 // If loading a variable font, you don't need to specify the font weight
 const montserrat = Montserrat({ subsets: ["latin"], variable: "--font-mont" });
 
 export default function App({ Component, pageProps }) {
+
   const router = useRouter();
 
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+      <Script id="google-analytics-script" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+          page_path: window.location.pathname,
+          });
+    `}
+      </Script>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main
-        className={`${montserrat.variable} font-mont  bg-light dark:bg-dark w-full min-h-screen h-full`}
-      >
+
+      <main className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen h-full`}>
         <Navbar />
         <AnimatePresence initial={false} mode="wait">
           <Component key={router.asPath} {...pageProps} />
         </AnimatePresence>
         <Footer />
       </main>
+
     </>
   );
 }
